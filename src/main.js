cd350f18d7f9c7293e73a9b4e9471a0ea7c1257a
AdrianPhuong <adrianphuong1@gmail.com>
2023-05-12 15:42:42 -0700

    creative tilt

--- src/main.js
+++ src/main.js
@@ -7,12 +7,22 @@ Simulate scrolling with a tileSprite (or equivalent means) (5) DONE
 Implement proper collision detection (via Arcade Physics or a custom routine) (5) DONE
 Have looping background music (5) DONE
 Use a minimum of three sound effects for key mechanics, UI, and/or significant events appropriate to your game design (5) DONE
-Use randomness to generate escalating challenge, e.g. terrain, pickups, etc. (5)
+Use randomness to generate escalating challenge, e.g. terrain, pickups, etc. (5) DONE
 Include some metric of accomplishment that a player can improve over time, e.g., score, survival time, etc. (5) DONE
 Be theoretically endless (5) DONE
 Be playable for at least 15 seconds for a new player of low to moderate skill (5) DONE
 Run without significant crashes or errors (5) DONE
 Include in-game credits for all roles, assets, music, etc. (5) DONE
+*/
+
+/*
+Creative Tilt
+For this assignment I tried to make it similar to the endless dinosaur chrome game with my own twist. This is the first
+time I tried using rotation effects to make it look like rocks were rolling towards the sprite.
+
+For the design, I really like how I made the environment revolve around a crab with the beach theme. Before with my
+Rocket Patrol mod assignment I had only used free assets so this was my first time actually doing a lot of in game art.
+
 */
 let config = {
   type: Phaser.CANVAS,
